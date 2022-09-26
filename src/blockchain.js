81bf5e4a8d8951fcbb64ac5834c6cce8fa2b488d
NAPPORN Omer <nappornomer@gmail.com>
2022-09-26 17:24:30 +0200

    requestMessageOwnershipVerification function

--- src/blockchain.js
+++ src/blockchain.js
@@ -91,7 +91,7 @@ class Blockchain {
      */
     requestMessageOwnershipVerification(address) {
         return new Promise((resolve) => {
-            
+            resolve(`${address}:${new Date().getTime().toString().slice(0,-3)}:starRegistry`)
         });
     }
 
